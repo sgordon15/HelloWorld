11a9221a4f4978531e0682d25dcc36d6fb10fa6d
kapst <kapster05@gmail.com>
2020-02-05 21:48:20 -0600

    changed the configuration

--- src/main/java/edu/bi/foo/HelloWorld.java
+++ src/main/java/edu/bi/foo/HelloWorld.java
@@ -2,11 +2,10 @@ package edu.bi.foo;
 
 public class HelloWorld {
     public static void main(String ...args){
-        String name = "bob";
-        if(args.length > 0){
-            name = args [0];
+        String name = (args.length > 0) ? args [0] :"bob"
+        System.out.println("hello, " + name + "from the world");
 
         }
-        System.out.println("hello, " + name + "from the world");
+
     }
 }
