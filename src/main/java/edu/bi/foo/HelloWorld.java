ef2eb00a508170541da3d146a298b8c6f3ff9e14
kapst <kapster05@gmail.com>
2020-02-05 21:46:12 -0600

    add program argument

--- src/main/java/edu/bi/foo/HelloWorld.java
+++ src/main/java/edu/bi/foo/HelloWorld.java
@@ -1,4 +1,12 @@
 package edu.bi.foo;
 
 public class HelloWorld {
+    public static void main(String ...args){
+        String name = "bob";
+        if(args.length > 0){
+            name = args [0];
+
+        }
+        System.out.println("hello, " + name + "from the world");
+    }
 }
